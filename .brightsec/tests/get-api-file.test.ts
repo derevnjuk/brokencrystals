a6a64efe79ac7d4624d4068fc0b138f0d914ba2f
Artem Derevnjuk <artedere@gmail.com>
2025-05-31 13:12:25 +0400

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-file.test.ts
+++ .brightsec/tests/get-api-file.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/file', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['lfi', 'rfi', 'open_cloud_storage', 'ssrf', 'csrf', 'unvalidated_redirect'],
+      tests: ['ssrf', 'lfi', 'rfi'],
       attackParamLocations: [AttackParamLocation.QUERY]
     })
     .threshold(Severity.CRITICAL)
--- .brightsec/tests/get-api-products.test.ts
+++ .brightsec/tests/get-api-products.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/products', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['sqli', 'date_manipulation', 'jwt', 'csrf', 'xss'],
+      tests: ['jwt'],
       attackParamLocations: [AttackParamLocation.QUERY, AttackParamLocation.HEADER],
       skipStaticParams: false
     })
