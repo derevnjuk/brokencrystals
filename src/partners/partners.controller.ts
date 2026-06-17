b6dc1b233a0f4c3f8982111a88c73b99b16f814b
Artsem Dzerauniuk <artem.derevnjuk@brightsec.com>
2026-06-17 14:35:17 +0400

    feat: implement getPartnerProfile endpoint

--- src/partners/partners.controller.ts
+++ src/partners/partners.controller.ts
@@ -109,6 +109,29 @@ export class PartnersController {
     }
   }
 
+  // **** IDOR: no authorization check ****
+  @Get(':partnerId/profile')
+  @Header('content-type', 'text/xml')
+  @ApiOperation({
+    description: 'Get partner profile by ID'
+  })
+  async getPartnerProfile(
+    @Param('partnerId') partnerId: string
+  ): Promise<string> {
+    this.logger.debug(`Loading profile for partner ID ${partnerId}`);
+
+    try {
+      const xpath = `//partners/partner[id/text()='${partnerId}']/*`;
+
+      return this.partnersService.getPartnersProperties(xpath);
+    } catch (err) {
+      throw new HttpException(
+        `Failed to load partner profile. Details: ${err}`,
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
   // **** This is a string based XPATH injection EP ****
   @Get('searchPartners')
   @ApiQuery({
